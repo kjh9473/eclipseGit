ddea1521856a55c3dadc0f42f41eef61ef976058
jhkim <rla08643@gmail.com>
2018-10-10 17:47:10 +0900

    second commit

--- src/main/java/kr/or/ddit/web/BasicServlet.java
+++ src/main/java/kr/or/ddit/web/BasicServlet.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+//이클립스 git second commit
+
 @WebServlet("/BasicServlet")
 public class BasicServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
